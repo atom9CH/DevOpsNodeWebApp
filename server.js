4c8c4df65a36e22a15ddda1a50f824d33a6105d1
atom9CH <rucksal1@students.zhaw.ch>
2026-04-10 18:35:05 +0200

    Changes LN4_ARU

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FS2026 DevOps Course!');
+  res.send('Hello FS2026 DevOps Course! This is a Node.js application running in a Docker container. Aufgabe 4');
 });
 
 app.listen(PORT, HOST);
