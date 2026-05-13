ca851785c3cd6e0affa04872276c971d221dde84
atom9CH <rucksal1@students.zhaw.ch>
2026-05-13 14:51:20 +0200

    Versuch 2

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FS2026 DevOps Course! Pipeline Commit erster Versuch');
+  res.send('Hello FS2026 DevOps Course! Pipeline Commit Versuch 2');
 });
 
 app.listen(PORT, HOST);
