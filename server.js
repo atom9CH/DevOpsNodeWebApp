952c89ec34f15831cde3fe0a38042e54cb82957d
atom9CH <rucksal1@students.zhaw.ch>
2026-05-13 16:34:07 +0200

    ProblemFix

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FS2026 DevOps Course! Pipeline Commit Versuch für Video');
+  res.send('TEST');
 });
 
 app.listen(PORT, HOST);
