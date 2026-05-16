6b0dd502d6029cf6420d5864f75fc1bf00732327
atom9CH <rucksal1@students.zhaw.ch>
2026-05-16 15:12:52 +0200

    UpdatedFile

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('LN10_GitHub_Action_TestVideo');
+  res.send('LN10_GitHub_Action_TestVideo_Nr2');
 });
 
 app.listen(PORT, HOST);
